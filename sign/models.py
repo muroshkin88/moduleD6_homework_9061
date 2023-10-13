21fb1271b54d39bb62f53225dd62f3cf0b001c17
boybest <125255781+muroshkin88@users.noreply.github.com>
2023-10-13 10:37:24 +0300

    Add files via upload

---
+++ sign/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
+
+class BasicSignupForm(SignupForm):
+
+    def save(self, request):
+        user = super(BasicSignupForm, self).save(request)
+        common_group = Group.objects.get(name='authors')
+        common_group.user_set.add(user)
+        return user
